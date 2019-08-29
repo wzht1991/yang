9b25d12c72e4e28c0416d39b30058ffe615d93d4
wzht1991 <506486189@qq.com>
2019-08-29 16:12:52 +0800

    use helmet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
 const express= require("express");
+const helmet = require('helmet')
 const app =express();
+const PORT=process.env.PORT||3000;
 
+app.use(helmet())
 app.get('/',(req,res)=>{
     res.send("welcome to our system")
 });
 app.listen(3000, ()=>{
-    console.log("xixiix")
+    console.log(`runing in ${PORT}`)
 });
+
+module.exports=app;
